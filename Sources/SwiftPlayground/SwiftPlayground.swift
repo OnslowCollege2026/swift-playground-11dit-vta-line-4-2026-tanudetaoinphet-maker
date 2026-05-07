f2a1a74a64cd40b7ecc2d92115f742f662a8f04c
Tanudet Aonphet <tanudet.aoinphet@student.onslow.school.nz>
2026-05-07 23:57:27 +0000

    May 7, 2026, 11:57 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,25 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
-    }
+        // For loop
+(1...10).forEach { number in
+   print(number * 2)
+}
+
+// While loop
+var number = 1
+repeat {
+   print(number * 2)
+number = number + 1
+} while number <= 10
+let items = ["Potion", "Ether", "Repel"]
+
+items.forEach { item in
+    print(item)
+}
+
+
+        
+        
+            }
 }
